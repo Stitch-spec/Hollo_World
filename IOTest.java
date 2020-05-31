da5446151ef1d2829648e9520fcdb12006db2a39
sky <58350191+Stitch-spec@users.noreply.github.com>
2020-05-31 15:43:05 +0800

    Add files via upload

---
+++ IOTest.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+
+import java.io.*;
+
+public class IOTest {
+    @Test
+    public void test01(){
+        OutputStream out = null;
+        OutputStream output = null;
+        try {
+            out = new FileOutputStream("pom.txt",true);
+            output = new BufferedOutputStream(out);
+            String s = "01234";
+//            out.write(s.getBytes());
+            output.write(s.getBytes());
+//            out.flush();
+            output.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void test02(){
+        OutputStream output = null;
+        InputStream input = null;
+        try {
+//            in = new FileInputStream("pom.xml");
+//            out = new FileOutputStream("pom.txt");
+            input = new BufferedInputStream(new FileInputStream("pom.xml"));
+            output = new BufferedOutputStream(new FileOutputStream("pom.txt"));
+            byte[] b = new byte[1024];
+            input.read(b);
+            String a = "haha";
+            output.write(b);
+            output.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
